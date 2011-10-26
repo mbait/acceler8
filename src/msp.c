0d9d3414f4217259ea5e5a8571ae3a4ba68aadcf
Alexander Solovets <asolovets@gmail.com>
2011-10-27 09:21:30 +1100

    Add fake row to the matrix to get rid of special cases.

--- src/msp.c
+++ src/msp.c
@@ -37,7 +37,7 @@ struct ans_s {
 };
 
 
-int a[MAX_N][MAX_N];
+int a[MAX_N][MAX_N + 1];
 int b[THREAD_NUM][MAX_N];
 
 struct test_s test[MAX_T];
@@ -56,7 +56,7 @@ inline void gen(int t)
 	long long sum = 0;
 
 	for (i = 0; i < test[t].nr; ++i) {
-		for (j = 0; j < test[t].nc; ++j) {
+		for (j = 1; j <= test[t].nc; ++j) {
 			seed = rand(seed, t);
 			a[i][j] = seed;
 			sum += seed;
@@ -67,19 +67,17 @@ inline void gen(int t)
 
 	#pragma omp parallel for private(j)
 	for (i = 0; i < test[t].nr; ++i) {
-		a[i][0] -= mean;
-
-		for (j = 1; j < test[t].nc; ++j)
+		for (j = 1; j <= test[t].nc; ++j)
 			a[i][j] += a[i][j - 1] - mean;
 	}
 
 #ifdef DEBUG
 	// Print accumulated matrix.
 	for (i = 0; i < test[t].nr; ++i) {
-		for (j = 0; j < test[t].nc; ++j)
+		for (j = 1; j <= test[t].nc; ++j)
 			fprintf(stderr, "%3d", a[i][j]);
 
-		putchar('\n');
+		fputc('\n', stderr);
 	}
 #endif
 }
@@ -96,13 +94,10 @@ void solve(int t)
 		tid = omp_get_thread_num();
 
 		#pragma omp for private(i, k, min_i, max_l, max_r, min, max, sum, cur)
-		for (j = 0; j < test[t].nc; ++j) {
-			for (k = j; k < test[t].nc; ++k) {
+		for (j = 1; j <= test[t].nc; ++j) {
+			for (k = j; k <= test[t].nc; ++k) {
 				for (i = 0; i < test[t].nr; ++i) {
-					b[tid][i] = a[i][k];
-
-					if (j > 0)
-						b[tid][i] -= a[i][j - 1];
+					b[tid][i] = a[i][k] - a[i][j - 1];
 				}
 
 				max = 0;
@@ -133,9 +128,9 @@ void solve(int t)
 					if (max >= ans[t].sum) {
 						ans[t].sum = max;
 						ans[t].r0 = max_l;
-						ans[t].c0 = j;
+						ans[t].c0 = j - 1;
 						ans[t].r1 = max_r;
-						ans[t].c1 = k;
+						ans[t].c1 = k - 1;
 					}
 				}
 			}

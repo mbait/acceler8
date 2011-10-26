c652c8e1cf013185ab4567af4fb82e031998fba3
Alexander Solovets <asolovets@gmail.com>
2011-10-26 15:39:05 +1100

    Speed-up solution.

--- src/msp.c
+++ src/msp.c
@@ -1,16 +1,28 @@
 /* Description: solution of the maximum subarray problem for two
  * dimensions. The solution employs Jay Kadane's algorithm which
- * has O(n^3) complexity. Optimzation techniques - OpenMP.
+ * has O(n) complexity. Parallelization technique - OpenMP.
  *
  * Author: Alexander Solovets <asolovets<at>gmail.com>
  */
 #include <stdio.h>
 #include <string.h>
 
+#ifndef _OPENMP
+static int omp_get_thread_num()
+{
+	return 0;
+}
+#else
+#include <omp.h>
+#endif
+
+
 #define SQR(t) ((ans[t].r1 - ans[t].r0 + 1) * (ans[t].c1 - ans[t].c0 + 1))
 
 #define MAX_N	20000
 #define MAX_T	1000
+
+#define THREAD_NUM	100
 
 
 struct test_s {
@@ -26,18 +38,17 @@ struct ans_s {
 
 
 int a[MAX_N][MAX_N];
-int b[MAX_N];
+int b[THREAD_NUM][MAX_N];
 
 struct test_s test[MAX_T];
 struct ans_s ans[MAX_T];
-
 
 inline int rand(int seed, int i)
 {
 	return (test[i].a * seed + test[i].b) % test[i].m;
 }
 
-inline void fill(int t)
+inline void gen(int t)
 {
 	int i, j;
 	int mean;
@@ -54,13 +65,23 @@ inline void fill(int t)
 
 	mean = ((long double)sum / (test[t].nr  * test[t].nc)) + 0.5;
 
-#pragma omp parallel for shared(j)
+	#pragma omp parallel for private(j)
 	for (i = 0; i < test[t].nr; ++i) {
 		a[i][0] -= mean;
 
 		for (j = 1; j < test[t].nc; ++j)
 			a[i][j] += a[i][j - 1] - mean;
 	}
+
+#ifdef DEBUG
+	// Print accumulated matrix.
+	for (i = 0; i < test[t].nr; ++i) {
+		for (j = 0; j < test[t].nc; ++j)
+			fprintf(stderr, "%3d", a[i][j]);
+
+		putchar('\n');
+	}
+#endif
 }
 
 void solve(int t)
@@ -68,21 +89,20 @@ void solve(int t)
 	int i, j, k;
 	int min_i, max_l, max_r;
 	long long min, max, sum, cur;
-	struct ans_s tans;
+	size_t tid;
 
-	fill(t);
+	#pragma omp parallel private(tid)
+	{
+		tid = omp_get_thread_num();
 
-#pragma omp parallel for private(b, k, i, min_i, max_l, max_r, min, max, sum, cur)
-//#pragma omp parallel
-	//{
-//#pragma omp parallel for firstprivate(k) lastprivate(k) reduction(-: b)
+		#pragma omp for private(i, k, min_i, max_l, max_r, min, max, sum, cur)
 		for (j = 0; j < test[t].nc; ++j) {
 			for (k = j; k < test[t].nc; ++k) {
 				for (i = 0; i < test[t].nr; ++i) {
-					b[i] = a[i][k];
+					b[tid][i] = a[i][k];
 
 					if (j > 0)
-						b[i] -= a[i][j - 1];
+						b[tid][i] -= a[i][j - 1];
 				}
 
 				max = 0;
@@ -93,7 +113,7 @@ void solve(int t)
 				min_i = -1;
 
 				for (i = 0; i < test[t].nr; ++i) {
-					sum += b[i];
+					sum += b[tid][i];
 					cur = sum - min;
 
 					if (cur >= max) {
@@ -108,17 +128,19 @@ void solve(int t)
 					}
 				}
 
-#pragma omp critical (ans)
-				if (max >= ans[t].sum) {
-					ans[t].sum = max;
-					ans[t].r0 = max_l;
-					ans[t].c0 = j;
-					ans[t].r1 = max_r;
-					ans[t].c1 = k;
+				#pragma omp critical(ans)
+				{
+					if (max >= ans[t].sum) {
+						ans[t].sum = max;
+						ans[t].r0 = max_l;
+						ans[t].c0 = j;
+						ans[t].r1 = max_r;
+						ans[t].c1 = k;
+					}
 				}
 			}
 		}
-//	}
+	}
 }
 
 int main(int argc, char **argv)
@@ -140,8 +162,10 @@ int main(int argc, char **argv)
 
 	fclose(f);
 
-	for (t = 0; t < tn; ++t)
+	for (t = 0; t < tn; ++t) {
+		gen(t);
 		solve(t);
+	}
 
 	f = fopen(argv[2], "w");
 

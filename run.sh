515c6d4517f682863fe4e19e73b3161ab5d41cfd
Alexander Solovets <asolovets@gmail.com>
2011-10-27 15:41:04 +1100

    Save test output.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 JOBID=${PBS_JOBID:-$$}
 DIST_DIR=${PBS_O_WORKDIR:-$PWD}
-DIST_DIR=$DIST_DIR sh $DIST_DIR/util/test.sh $DIST_DIR/out/msp_rtl > $DIST_DIR/test.out.$JOBID
+PID=$JOBID DIST_DIR=$DIST_DIR sh $DIST_DIR/util/test.sh $DIST_DIR/out/msp_rtl > $DIST_DIR/test.out.$JOBID
--- util/test.sh
+++ util/test.sh
@@ -8,13 +8,16 @@ die()
 }
 
 WD=${DIST_DIR:-$PWD}
+PID=${PID:-$$}
+OUT_DIR=$WD/test/out.$PID
 
 [ "$#" -eq 1 ]  && test -x $1 || die 'usage: sh test.sh <executable>'
-test -d $WD/test/out || mkdir $WD/test/out
+test -d $OUT_DIR || mkdir $OUT_DIR
 
 echo 'cells real user system'
 
 for f in $WD/test/*.in
 do
-	(time $1 $f ans > /dev/null) 2>&1 | tail -n+2 | perl $WD/util/time.pl $f
+	ans=$OUT_DIR/$(echo $f | sed 's/.in$/.ans/' | xargs basename)
+	(time $1 $f $ans > /dev/null) 2>&1 | tail -n+2 | perl $WD/util/time.pl $f
 done

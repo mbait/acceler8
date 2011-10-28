10326ccdaea37c5141e03ad2e00e42e54c217365
Alexander Solovets <asolovets@gmail.com>
2011-10-29 08:27:46 +1100

    Export required variables, set num threads with OMP_NUM_THREADS.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-JOBID=${PBS_JOBID:-$$}
-DIST_DIR=${PBS_O_WORKDIR:-$PWD}
-PID=$JOBID DIST_DIR=$DIST_DIR sh $DIST_DIR/util/test.sh $DIST_DIR/out/msp_rtl > $DIST_DIR/test.out.$JOBID
+export PID=${PBS_JOBID:-$$}
+export DIST_DIR=${PBS_O_WORKDIR:-$PWD}
+export MSP_EXEC=${1:-msp_rtl}
+export OMP_NUM_THREADS=${2:-30}
+sh $DIST_DIR/util/test.sh $DIST_DIR/out/$MSP_EXEC > $DIST_DIR/test.out.$PID

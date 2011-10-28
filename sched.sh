ecdc968ee91020b594c392bf60cf8a2f6efea158
Alexander Solovets <asolovets@gmail.com>
2011-10-29 10:24:44 +1100

    Swap paramaters in schedule script.

--- sched.sh
+++ sched.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NUM_CORES=${2:-40}
-MSP_EXE=${1:-msp_rtl}
+NUM_CORES=${1:-40}
+MSP_EXE=${2:-msp_rtl}
 
 qsub -l select=1:ncpus=$NUM_CORES -v MSP_EXE=$MSP_EXE,OMP_NUM_THREADS=$NUM_CORES ./run.sh

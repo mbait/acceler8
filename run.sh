f8b9deb1404bd6e72d520814d030ea681af5742f
Alexander Solovets <asolovets@gmail.com>
2011-10-29 11:26:37 +1100

    Append job parameters to the ouput file name.

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+NAME=${PBS_JOBNAME:-local}
 export PID=${PBS_JOBID:-$$}
 export DIST_DIR=${PBS_O_WORKDIR:-$PWD}
 export OMP_NUM_THREADS=$OMP_NUM_THREADS
-sh $DIST_DIR/util/test.sh $DIST_DIR/out/$MSP_EXE > $DIST_DIR/test.out.$PID
+sh $DIST_DIR/util/test.sh $DIST_DIR/out/$MSP_EXE > "$DIST_DIR/test.out.$PID.$NAME"
--- sched.sh
+++ sched.sh
@@ -2,4 +2,8 @@
 NUM_CORES=${1:-40}
 MSP_EXE=${2:-msp_rtl}
 
-qsub -l select=1:ncpus=$NUM_CORES -v MSP_EXE=$MSP_EXE,OMP_NUM_THREADS=$NUM_CORES ./run.sh
+qsub \
+	-l select=1:ncpus=$NUM_CORES \
+	-v MSP_EXE=$MSP_EXE,OMP_NUM_THREADS=$NUM_CORES \
+	-N "$MSP_EXE-$NUM_CORES" \
+	./run.sh

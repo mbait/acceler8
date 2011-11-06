8dc1016f2eacf2a382e9ea43d846a5a6ee327b58
Alexander Solovets <asolovets@gmail.com>
2011-11-07 10:23:21 +1100

    Construct job name with leading zeroes in the nubmber of CPUs used.

--- sched.sh
+++ sched.sh
@@ -5,5 +5,5 @@ MSP_EXE=${2:-msp_rtl}
 qsub \
 	-l select=1:ncpus=$NUM_CORES \
 	-v MSP_EXE=$MSP_EXE,OMP_NUM_THREADS=$NUM_CORES \
-	-N "$MSP_EXE-$NUM_CORES" \
+	-N "$MSP_EXE-$(printf "%02d" "$NUM_CORES")" \
 	./run.sh

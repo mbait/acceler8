b13593c805a71064abd20bdfbca9fc72b6ebc05c
Alexander Solovets <asolovets@gmail.com>
2011-11-15 11:05:33 +1100

    Do not align the number of threads to two chars, set graphic height to 400px.

--- doc/img/gen_graph.pl
+++ doc/img/gen_graph.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-my $term = "pngcairo size 800,300 font 'terminus,9'";
+my $term = "pngcairo size 800,400 font 'terminus,9' rounded";
 my $line_style = "pt 5 ps 0.5 lt 1 lw 1.5";
 my @line_colors = qw(ff0033 0099ff 66cc66 9966ff 666600 ff66ff);
 
@@ -19,4 +19,4 @@ print "set key l under\n";
 print "set style increment user\n";
 print "set style line $_ lc rgb '#$line_colors[$_-1]' $line_style\n" for 1..6;
 print "plot ", join(', ',
-	map { /0?(\d+)$/; sprintf "'$_' u 1:2 w lp t 'Количество потоков: %2d'", $1 } @src), "\n";
+	map { /0?(\d+)$/; sprintf "'$_' u 1:2 w lp t 'Количество потоков: %d'", $1 } @src), "\n";

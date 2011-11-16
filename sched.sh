3ec623774df82a25a735649b72e9a29cb8eda662
Alexander Solovets <asolovets@gmail.com>
2011-11-16 18:29:50 +1100

    Increase maximum wall time for scheduled tasks.

--- sched.sh
+++ sched.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 NUM_CORES=${1:-40}
 MSP_EXE=${2:-msp_rtl}
+TIME=00:30:00
 
 qsub \
-	-l select=1:ncpus=$NUM_CORES \
+	-l select=1:ncpus=$NUM_CORES,walltime=$TIME \
 	-v MSP_EXE=$MSP_EXE,OMP_NUM_THREADS=$NUM_CORES \
 	-N "$MSP_EXE-$(printf "%02d" "$NUM_CORES")" \
 	./run.sh
